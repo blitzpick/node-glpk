b32982bfa6bd6b06740f089167cd64e8c51069fb
David Carley <david@blitzpick.co>
2017-03-16 21:23:24 -0700

    Cleaned up example.

--- examples/jsonModel.js
+++ examples/jsonModel.js
@@ -2,16 +2,8 @@ var glp = require("..");
 var model = require("./model.json");
 var prob = new glp.Problem();
 
-console.time("load");
-for (var i = 0; i < 100; ++i) {
-  var prob = new glp.Problem();
-  prob.loadJsonModel(model);
-}
-console.timeEnd("load");
-
-console.time("load");
 prob.loadJsonModel(model);
-console.timeEnd("load");
+
 prob.scaleSync(glp.SF_AUTO);
 prob.simplexSync({
   presolve: glp.ON
@@ -26,5 +18,6 @@ if (prob.getNumInt() > 0) {
     cbFunc: callback
   });
 }
+
 console.log("objective: " + prob.mipObjVal());
 prob.delete();
